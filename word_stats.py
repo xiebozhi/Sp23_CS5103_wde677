17093cec629d6a75509f9537f6e084cc0b6740f1
Bobb Shields <bobbshields@gmail.com>
2023-04-02 10:10:23 -0500

    Second sprint - working code and singular unit tests for count_lines and count_chars

--- word_stats.py
+++ word_stats.py
@@ -3,13 +3,14 @@
 #
 
 # Stats for text passed in as the singular parameter
-#
+# First sprint: count_words
+# Second sprint: count_lines, count_chars
 #
 
 # Count of the individual words.  
 # All characters pushed to lowercase as to make this case insensitive
 #
- 
+import string
 
 def count_words(text):
     # replace tabs and newlines with spaces
@@ -36,3 +37,22 @@ def count_words(text):
                 word_counts[word] += 1
             
         return word_counts
+
+def count_lines (text): #looks for all “\n” tokens in the given string
+
+    line_count = 0
+    for i in range(0,len(text) ):
+        if text[i] == "\n": 
+            line_count = line_count + 1
+
+    return line_count
+
+def count_chars(text): #CharCount is simply the length of the text field.
+
+    print(f"len = {len(text)}")
+    char_count = 0
+    for char in text:
+        if char not in string.whitespace:
+            char_count += 1
+
+    return char_count
