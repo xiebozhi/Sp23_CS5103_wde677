664a8a64b6349f7753fedfb455672021a2e276cc
Bobb Shields <bobbshields@gmail.com>
2023-04-23 14:56:48 -0500

    refactor: docstrings instead of comments

--- word_stats.py
+++ word_stats.py
@@ -10,21 +10,35 @@
 import string
 import re
 
-# Combine function calls with same parameter together to print and label 
-# Just a courtesy 
-#
 def print_stats(text):
+    """Simple helper function to count lines & chars & words
 
+    Args:
+        text (string): Any given text: A paragraph with symbols, punctuation, numbers, and letters. 
+
+    Returns:
+        null: Prints a summary of the three functions.  Used in main.py
+    """
     return ("Num Lines: " + str(count_lines(text)) +"\n" +
             "Num Chars: " + str(count_chars(text)) + "\n" +
             "Word Counts by freq:\n" + str(count_words(text) )
         )
 
-# Global input bounds checker that throws specific errors if failed  
-# 
-#
 def check_input(text):
+    """Global input bounds checker that throws specific errors if failed
 
+    Args:
+        text (string): Any given text: A paragraph with symbols, punctuation, numbers, and letters. 
+
+    Raises:
+        TypeError: Check for string or list
+        TypeError: When a list - check elements for whether string
+        ValueError: Cannot be empty
+        ValueError: Exceding maximum text length
+
+    Returns:
+        boolean: Returns true, if no errors were thrown
+    """
     #if input is a single string, save as a list
     if isinstance(text, str):
         text_list = [text]
@@ -46,9 +60,15 @@ def check_input(text):
     
     return True
 
-def to_upper_or_lower(validated_text, caseParam):
+def to_upper_or_lower(validated_text, toCase):
+    """Transform the text to lower- or uppercase
+
+    Args:
+        validated_text (string): Any given text: A paragraph with symbols, punctuation, numbers, and letters. Should already be checked by check_input
+        toCase (string): Flags: {'lower', 'upper', 'sensitive'}
+    """
      #check case
-    match caseParam:
+    match toCase:
         case "lower":
             validated_text= validated_text.lower()
         case "upper":
@@ -57,18 +77,25 @@ def to_upper_or_lower(validated_text, caseParam):
             #do nothing
             print("")
         case _:
-            print("Case parameter = "+ caseParam + ". Not performing upper or lower conversion, defaulting to case sensitive.")
+            print("Case parameter = "+ toCase + ". Not performing upper or lower conversion, defaulting to case sensitive.")
     return validated_text
 
-# Count of the individual words.  
-# All characters pushed to lowercase as to make this case insensitive
-#
-def count_words(text,caseParam ='lower', tokenizers=["\t","\n"," "] ):
+def count_words(text,toCase ='lower', tokenizers=["\t","\n"," "] ):
+    """Count of the individual words.  
+
+    Args:
+        text (string): Any given text: A paragraph with symbols, punctuation, numbers, and letters. 
+        toCase (str, optional): Transform text to 'upper' or 'lower' case, or stay case 'sensitive'. Defaults to 'lower'.
+        tokenizers (list, optional): User provided list of tokens upon which to split the text. Defaults to ["\t","\n"," "].
+
+    Returns:
+        dictionary: The individual words and their counts, sorted from highest to lowest
+    """
     #check input text and it will throw errors if needed
     check_input(text)
     
     #Convert text to upper or lower, if specified in the parameters
-    text = to_upper_or_lower(text, caseParam)
+    text = to_upper_or_lower(text, toCase)
         
     for token in tokenizers:   
         # replace tabs and newlines with spaces
@@ -90,10 +117,15 @@ def count_words(text,caseParam ='lower', tokenizers=["\t","\n"," "] ):
 
     return sorted_word_counts
 
-# Count of the lines.  
-# number of new line characters + 1 
-#
 def count_lines (text): #looks for all \n tokens in the given string
+    """Count of the lines: num(newline chars) + 1
+
+    Args:
+        text (string): Any given text: A paragraph with symbols, punctuation, numbers, and letters. 
+
+    Returns:
+        integer: Returns the number of lines in the text
+    """
     #check input and it will throw errors if needed
     check_input(text)
 
@@ -105,16 +137,21 @@ def count_lines (text): #looks for all \n tokens in the given string
 
     return line_count
 
+def count_chars(text, toCase ='lower'): 
+    """Count of the individual characters of the input text with whitespace removed (space, tab, linefeed, return, formfeed, and vertical tab)
 
-# Count of the individual characters
-# Whitespace removed (space, tab, linefeed, return, formfeed, and vertical tab)
-#
-def count_chars(text, caseParam ='lower'): 
+    Args:
+        text (string): Any given text: A paragraph with symbols, punctuation, numbers, and letters. 
+        toCase (str, optional): Transform text to 'upper' or 'lower' case, or stay case 'sensitive'. Defaults to 'lower'.
+
+    Returns:
+        integer: Returns the number of non-trivial characters in the text
+    """
     #check input and it will throw errors if needed
     check_input(text)
     
     #Convert text to upper or lower, if specified in the parameters
-    text = to_upper_or_lower(text, caseParam)
+    text = to_upper_or_lower(text, toCase)
 
     print(f"len = {len(text)}")
     char_count = 0
@@ -124,10 +161,19 @@ def count_chars(text, caseParam ='lower'):
 
     return char_count
 
-# Replace a word within a string.  
-# Uses regular expression to evaluate against the whole word
-# Adding ", flags=re.IGNORECASE" as arg to re.sub() affects case sensitivity 
+
+
 def replace_word(text, find, replace):
+    """# Replace a word within a string.  Uses regular expression to evaluate against the whole word
+
+    Args:
+        text (string): Any given text: A paragraph with symbols, punctuation, numbers, and letters. 
+        find (string): The word or body of text to be replaced
+        replace (string): The word or body of text to be substituted in 
+    
+    Returns:
+        string: Original text with keywords replaced
+    """
     #check inputs and it will throw errors if needed
     check_input( [text, find, replace] )
 
